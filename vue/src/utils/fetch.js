8d94115e9e08fef5af4ffbe8bf157212420e2787
罗旭 <loword@163.com>
2023-07-22 01:09:24 +0800

    vue路径修改

--- vue/src/utils/fetch.js
+++ vue/src/utils/fetch.js
@@ -6,7 +6,7 @@ import { getToken } from '@/utils/auth'
 // 创建axios实例
 const service = axios.create({
   /*baseURL: process.env.BASE_API,*/ // api的base_url
-  baseURL: "/TeamTalk/users",
+  baseURL: "/im/users",
   
   timeout: 5000                  // 请求超时时间
 })
